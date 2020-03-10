2b1b782763e773e77415aa89c1cb0ae708a64258
Arina Okshus <nikita@sushko.ru>
2020-03-10 10:53:45 +0300

    Fixed shit

--- main.py
+++ main.py
@@ -9,30 +9,31 @@ def get_book(path):
 def get_sheet(book, num):
     return book.sheet_by_index(num)
 
-def get_cells(sheet):
-    return [[sheet.cell(j, i) for j in range(60)] for i in range(7)]
-
-def get_group(cells, num):
-    return cells[num]
-
 def get_day(sheet, num):
-    day_dirty = [[i for i in sheet.row_values(j)] for j in range(2+num*9, 11+num*9)]
-    day = [day_dirty[0]]
-    for i in range(1, len(day_dirty) - 1, 2):
-        day.append(day_dirty[i][0])
-        if i == 0:
-            day.append(day_dirty[i])
-            continue
-        else:
-            for j in range(1, len(day_dirty[i])):
-                day.append([day_dirty[i][j], day_dirty[i+1][j]])
+    # Getting raw day data
+    day_dirty = [[i for i in sheet.row_values(j)] for j in range(2+num%10+num*10, 13+num%10+num*10)]
 
+    day = [day_dirty[0]]
+
+    for i in range(2, len(day_dirty), 2):
+        # Appending time
+        day.append([day_dirty[i-1][0]])
+        for j in range(1, len(day_dirty[i])):
+            day[i//2].append([day_dirty[i-1][j], day_dirty[i][j]])
     return day
+
+def get_group_day(day, num):
+    group = [day[0][0]]
+    for i in range(1, len(day)):
+        group.append([day[i][0], day[i][1 + int(num) - int(day[0][1])]])
+    return group
+
+def get_group(sheet, num):
+    return [get_group_day(get_day(sheet, i), num) for i in range(5)]
 
 printer = pp.PrettyPrinter()
 book = get_book('test.xls')
-second = get_sheet(book, 1)
-cells = get_cells(second)
-seven = get_group(cells, 1)
-day = get_day(second, 1)
-printer.pprint(day)
+sheet = get_sheet(book, 0)
+day = get_day(sheet, 1)
+group = get_group(sheet, 207)
+printer.pprint(group)
